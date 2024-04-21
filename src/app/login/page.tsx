c760318d49be00c1bc59cfc29179f39d44155fc4
Tweni47 <derrickayaya@gmail.com>
2024-04-22 00:24:39 +0300

    Signup and Login backend

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,25 +2,51 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 const LoginPage = () => {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
-  const onLogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      toast.error(error.message);
+      console.log("Login failed", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="mb-2">Login</h1>
+      <h1 className="mb-2">{loading ? "Processing" : "Login"}</h1>
       <hr />
 
       <div className="flex flex-col">
         <label htmlFor="email">Email</label>
         <input
-          className="py-2 pl-2 rounded mb-4 focus:outline-none"
+          className="py-2 pl-2 rounded mb-4 focus:outline-none text-black"
           id="email"
           type="text"
           value={user.email}
@@ -32,7 +58,7 @@ const LoginPage = () => {
       <div className="flex flex-col">
         <label htmlFor="password">Password</label>
         <input
-          className="py-2 pl-2 rounded mb-4 focus:outline-none"
+          className="py-2 pl-2 rounded mb-4 focus:outline-none text-black"
           id="password"
           type="password"
           value={user.password}
@@ -45,7 +71,7 @@ const LoginPage = () => {
         onClick={onLogin}
         className="py-2 px-4 border border-gray-300 rounded mb-4 hover:border-sky-500 hover:text-sky-500"
       >
-        Login
+        {buttonDisabled ? "No Login" : "Login"}
       </button>
       <span>
         Don&apos;t have an account?
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,26 +2,54 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 const SignupPage = () => {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
     username: "",
   });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const onSignup = async () => {};
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      toast.error(error.message);
+      console.log("Signup failed", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="mb-2">Signup</h1>
+      <h1 className="mb-2">{loading ? "Processing" : "Signup"}</h1>
       <hr />
 
       <div className="flex flex-col">
         <label htmlFor="username">Username</label>
         <input
-          className="py-2 pl-2 rounded mb-4 focus:outline-none"
+          className="py-2 pl-2 rounded mb-4 focus:outline-none text-black"
           id="username"
           type="text"
           value={user.username}
@@ -33,7 +61,7 @@ const SignupPage = () => {
       <div className="flex flex-col">
         <label htmlFor="email">Email</label>
         <input
-          className="py-2 pl-2 rounded mb-4 focus:outline-none"
+          className="py-2 pl-2 rounded mb-4 focus:outline-none text-black"
           id="email"
           type="text"
           value={user.email}
@@ -45,7 +73,7 @@ const SignupPage = () => {
       <div className="flex flex-col">
         <label htmlFor="assword">Password</label>
         <input
-          className="py-2 pl-2 rounded mb-4 focus:outline-none"
+          className="py-2 pl-2 rounded mb-4 focus:outline-none text-black"
           id="password"
           type="password"
           value={user.password}
@@ -58,7 +86,7 @@ const SignupPage = () => {
         onClick={onSignup}
         className="py-2 px-4 border border-gray-300 rounded mb-4 hover:border-sky-500 hover:text-sky-500"
       >
-        Sign Up
+        {buttonDisabled ? "No Signup" : "Signup"}
       </button>
       <span>
         Already have an account?
